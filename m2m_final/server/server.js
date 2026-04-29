2829e2fc0186784f5275448f24bfdf91c35370a5
Het-Vegad <155999196+Het-Vegad@users.noreply.github.com>
2026-04-29 13:33:45 +0530

    Update server.js

--- m2m_final/server/server.js
+++ m2m_final/server/server.js
@@ -14,10 +14,7 @@ connectDB();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://mistaketomagic.vercel.app"
-  ],
+  origin: process.env.CLIENT_URL || 'https://mistake-to-magic.vercel.app/',
   credentials: true
 }));
 
