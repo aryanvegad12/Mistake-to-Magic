6832f6d44ea833704eb179b284c3ab8c45357183
Het-Vegad <155999196+Het-Vegad@users.noreply.github.com>
2026-04-29 13:00:56 +0530

    Update api.js

--- m2m_final/client/src/utils/api.js
+++ m2m_final/client/src/utils/api.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' }
 });
 
-// Attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('m2m_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 }, (error) => Promise.reject(error));
 
-// Handle 401 globally
 api.interceptors.response.use(
   (res) => res,
   (error) => {
